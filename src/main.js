cf7e24bc470f84458ecb80a71a555f6c4e8235ad
Rico <superjohn673@gmail.com>
2024-07-19 17:10:38 +0800

    create env

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// 引入 Bootstrap 的 CSS
+import "bootstrap/dist/css/bootstrap.css";
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
+  { 
     path: '/',
     name: 'home',
     component: HomeView
@@ -13,7 +13,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
